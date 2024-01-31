fdc0e1cc4cb19bceb95d949ad0df56fd4df5ee0f
Lorena Infante <m.lorenainfanter@gmail.com>
2024-01-31 12:19:44 -0300

    creating a new comp in folder server

---
+++ the-basics/src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-server', //this must be unique
+    template:'./server.component.html' //external file
+})
+
+export class ServerComponent{};
