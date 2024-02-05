96b0a936af62417c56f68c766757fa7ec9e11ae7
Lorena Infante <m.lorenainfanter@gmail.com>
2024-02-05 15:45:25 -0300

    adding a new component via CLI

--- the-basics/src/app/app.module.ts
+++ the-basics/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { LoreComponent } from './lore/lore.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    LoreComponent
+    LoreComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
