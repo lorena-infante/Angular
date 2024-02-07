99fe6db81a4a2fb11799b0a3faf57c2e441b0a1b
Lorena Infante <m.lorenainfanter@gmail.com>
2024-02-07 16:21:24 -0300

    adding css as stlyleUrls adn example of inline styles

--- the-basics/src/app/servers/servers.component.ts
+++ the-basics/src/app/servers/servers.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
+  template: `<p class="h1 text-success">servers works!</p>
+  <app-server></app-server>
+  <app-server></app-server>
+  <lore-app></lore-app>`,
+  //external file styles
+  styleUrls: ['./servers.component.css']
+  // inline styles
+/*   styles: [`p {
+    color:darkorange;
+    font-size: large;
+    font-weight: 700;
+}`] */
 })
 export class ServersComponent {
 
