2b88a11e4b4c96c62dcc2950402e5430e5155749
Lorena Infante <m.lorenainfanter@gmail.com>
2024-02-07 18:22:58 -0300

    renaming warning-app to app-warning-alert

--- first-assignment/src/app/app.module.ts
+++ first-assignment/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WarningComponent } from './warning-alert/warning.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningComponent,
+    WarningAlertComponent,
     SuccessAlertComponent
   ],
   imports: [
--- first-assignment/src/app/warning-alert/warning.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector:'warning-app',
-    templateUrl:'./warning.component.html',
-    styleUrls:['./warning.component.css']
-})
-
-export class WarningComponent{};
