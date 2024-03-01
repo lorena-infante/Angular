f5e3be2eaf882dca3b4dd904fc7dfdf3e417a314
Lorena Infante <m.lorenainfanter@gmail.com>
2024-03-01 16:42:47 -0300

    added offline class on server.component.html and .ts

--- the-basics/src/app/server/server.component.ts
+++ the-basics/src/app/server/server.component.ts
@@ -1,7 +1,22 @@
 import { Component } from "@angular/core";
 @Component({
     selector:'app-server', //this must be unique. Any name.
-    templateUrl:'./server.component.html' //external file
+    templateUrl:'./server.component.html', //external file
+    styles:[`
+    p {
+        &.online {
+            font-size: 2rem;
+            font-weight: 700;
+            color: #BEEE62
+        }
+        
+        &.offline {
+            font-size: 2rem;
+            font-weight: 700;
+            color: #483C46
+        }
+    }
+    `]
 })
 
 export class ServerComponent{
